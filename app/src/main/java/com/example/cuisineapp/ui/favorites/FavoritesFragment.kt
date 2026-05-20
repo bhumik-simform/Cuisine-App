48e5703b4cb5dbc078b64f76e3ddd197a0661ed7
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-05-20 11:25:14 +0530

    ✨ Implement ViewPager navigation with fragment transitions

--- app/src/main/java/com/example/cuisineapp/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/cuisineapp/ui/favorites/FavoritesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.cuisineapp.ui.settings
+package com.example.cuisineapp.ui.favorites
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,12 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.cuisineapp.R
 
-class SettingsFragment : Fragment() {
+class FavoritesFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
     }
 }
