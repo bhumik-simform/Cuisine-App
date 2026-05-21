1063227fe2a98f20d69d624fd652a84d02b92cdb
Bhumik91 <poshiyabhumik@gmail.com>
2026-05-21 07:50:10 +0530

    ✨ Implement Favorite Fragment List

--- app/src/main/java/com/example/cuisineapp/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/cuisineapp/ui/favorites/FavoritesFragment.kt
@@ -9,8 +9,15 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cuisineapp.R
 import com.example.cuisineapp.data.DishRepository
+import com.example.cuisineapp.data.FavoriteManger
+import com.example.cuisineapp.model.Dish
+import com.example.cuisineapp.ui.details.DetailsFragment
+import com.example.cuisineapp.ui.home.DishListAdapter
 
 class FavoritesFragment : Fragment() {
+
+    private lateinit var favoriteManger: FavoriteManger
+    private lateinit var adapter: DishListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,10 +29,53 @@ class FavoritesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        favoriteManger = FavoriteManger(requireContext())
         val favoritesRecyclerView: RecyclerView = view.findViewById(R.id.recycler_view_favorites)
-        favoritesRecyclerView.layoutManager = LinearLayoutManager(context)
-        favoritesRecyclerView.adapter = FavoritesAdapter(DishRepository.getAllDishes()) { dishID ->
-            print(dishID)
+        val emptyView: View = view.findViewById(R.id.view_empty_state)
+
+        val onDishClicked: (Dish) -> Unit = { clickedDish ->
+            val detailsFragment = DetailsFragment()
+
+            val bundle = Bundle()
+            bundle.putString("DISH_ID", clickedDish.idMeal)
+
+            detailsFragment.arguments = bundle
+
+            requireActivity().supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.detail_fragment_Container, detailsFragment)
+                .addToBackStack(null)
+                .commit()
         }
+
+        val onFavoriteIconClick: (Dish) -> Unit = { clickedDish ->
+            favoriteManger.toggleFavorite(clickedDish.idMeal)
+
+            loadFavoriteDishes(favoritesRecyclerView,emptyView)
+        }
+
+        adapter = DishListAdapter(onDishClicked, onFavoriteIconClick)
+
+        favoritesRecyclerView.layoutManager = LinearLayoutManager(context)
+        favoritesRecyclerView.adapter = adapter
+
+        loadFavoriteDishes(favoritesRecyclerView,emptyView)
+    }
+
+    private fun loadFavoriteDishes(recyclerView: RecyclerView, emptyView: View) {
+        val allFavoriteIds = favoriteManger.getFavoritesIds()
+
+        val favoriteDishes = DishRepository.getAllDishes().filter { allFavoriteIds.contains(it.idMeal) }
+
+        if (favoriteDishes.isEmpty()) {
+          emptyView.visibility = View.VISIBLE
+            recyclerView.visibility = View.GONE
+        } else {
+            recyclerView.visibility = View.VISIBLE
+            emptyView.visibility = View.GONE
+        }
+
+        adapter.submitList(favoriteDishes)
     }
 }
--- app/src/main/java/com/example/cuisineapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cuisineapp/ui/home/HomeFragment.kt
@@ -1,17 +1,23 @@
 package com.example.cuisineapp.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cuisineapp.R
 import com.example.cuisineapp.data.DishRepository
+import com.example.cuisineapp.data.FavoriteManger
+import com.example.cuisineapp.model.Dish
+import com.example.cuisineapp.ui.details.DetailsFragment
 
 class HomeFragment : Fragment() {
+
+    private lateinit var favoriteManger: FavoriteManger
+    private lateinit var adapter: DishListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,14 +30,49 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Load dishes if they haven't been loaded yet
-        DishRepository.loadDishes(requireContext())
+        favoriteManger = FavoriteManger(requireContext())
+
+        val onDishClick: (Dish) -> Unit = { clickedDish ->
+
+            val detailsFragment = DetailsFragment()
+
+            val bundle = Bundle()
+            bundle.putString("DISH_ID",clickedDish.idMeal)
+
+            detailsFragment.arguments = bundle
+
+            requireActivity().supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.detail_fragment_Container, detailsFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        val onFavoriteClick: (Dish) -> Unit = { clickedDish ->
+            favoriteManger.toggleFavorite(clickedDish.idMeal)
+
+            val index = adapter.currentList.indexOf(clickedDish)
+            if(index != -1) {
+                adapter.notifyItemChanged(index)
+            }
+        }
+
+        adapter = DishListAdapter(onDishClick,onFavoriteClick)
+
 
         val homeRecyclerView: RecyclerView = view.findViewById(R.id.recycler_view_home)
         homeRecyclerView.layoutManager = LinearLayoutManager(context)
-        homeRecyclerView.adapter = DishAdapter(DishRepository.getAllDishes()) { dishID ->
-            print(dishID)
-            
+        homeRecyclerView.adapter = adapter
+
+        val allDishes = DishRepository.getAllDishes()
+        adapter.submitList(allDishes)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onResume() {
+        super.onResume()
+        if(::adapter.isInitialized) {
+            adapter.notifyDataSetChanged()
         }
     }
 }
