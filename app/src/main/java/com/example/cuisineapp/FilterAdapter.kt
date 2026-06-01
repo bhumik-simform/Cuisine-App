b6e52167567aa7d3360cfba6da890d513d633114
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-06-01 14:58:46 +0530

    ✨ Implement Splash Screen for the app.

---
+++ app/src/main/java/com/example/cuisineapp/FilterAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.cuisineapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cuisineapp.FilterAdapter.FilterViewHolder
+import com.example.cuisineapp.model.Dish
+import com.google.android.material.chip.Chip
+
+class FilterAdapter(
+    private val dishCountriesList: List<String>,
+    private val filterChipClicked: (Chip) -> Unit
+): RecyclerView.Adapter<FilterViewHolder>() {
+    override fun onCreateViewHolder(
+        p0: ViewGroup,
+        p1: Int
+    ): FilterViewHolder {
+        val itemView = LayoutInflater.from(p0.context).inflate(R.layout.item_filters,p0,false)
+        return FilterViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(
+        p0: FilterViewHolder,
+        p1: Int
+    ) {
+        val item = dishCountriesList.elementAt(p1)
+        p0.bindData(item)
+    }
+
+    override fun getItemCount() = dishCountriesList.count()
+
+    inner class FilterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val filterChip: Chip = itemView.findViewById(R.id.chip_filter)
+
+        fun bindData(itemCountry: String) {
+            filterChip.text = itemCountry
+           filterChip.setOnClickListener {
+               filterChipClicked(filterChip)
+           }
+        }
+    }
+}
--- app/src/main/java/com/example/cuisineapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/cuisineapp/ui/details/DetailsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.cuisineapp.R
 import com.example.cuisineapp.data.DishRepository
@@ -84,7 +85,7 @@ class DetailsFragment : Fragment() {
         }
 
         backBtn.setOnClickListener {
-            requireActivity().supportFragmentManager.popBackStack()
+            findNavController().popBackStack()
         }
     }
 
